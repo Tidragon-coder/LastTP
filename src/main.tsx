e283296ac823beb6b6587e40185390b95685f244
Tidragon <theomoon77@gmail.com>
2025-08-11 15:26:47 +0200

    Finishing the installation, setting up Helmet (not compatible with React19 at the moment)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async';
 import './index.css'
 import Home from './pages/home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <HelmetProvider>
+      <Home />
+    </HelmetProvider>
   </StrictMode>,
 )
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,15 @@
-
+import { Helmet} from 'react-helmet-async';
 export default function Home() {
-  return (
-    <div>
-      <h2 className="text-2xl text-red-500">Welcome to LastRP</h2>
-      <p>This is the home page of the LastRP application.</p>
-    </div>
-  );
+    return (
+        <div>
+            <Helmet>
+                <title>LastRP - Home</title>
+                <meta name="description" content="Welcome to LastRP, the home page of our application." />
+            </Helmet>
+            <div>
+                <h2 className="text-2xl text-red-500">Welcome to LastRP</h2>
+                <p>This is the home page of the LastRP application.</p>
+            </div>
+        </div>
+    );
 }
