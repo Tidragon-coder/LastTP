df743ca53908026408964663cd76c5b28d1d380d
Tidragon <theomoon77@gmail.com>
2025-08-11 15:07:16 +0200

    First commit - init project

---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-2xl text-red-500">Welcome to LastRP</h2>
+      <p>This is the home page of the LastRP application.</p>
+    </div>
+  );
+}
