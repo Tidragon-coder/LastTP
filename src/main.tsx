d7c480485c11b80ae93c87122bcaa7d798bac7ff
Tidragon <theomoon77@gmail.com>
2025-09-18 20:17:17 +0200

    import react-scrollbars-custom, and custom scrollbars style

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async';
+import { Scrollbar } from "react-scrollbars-custom";
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <Scrollbar
+        style={{ width: "100vw", height: "100vh" }}
+        trackYProps={{
+          style: {
+            background: "#030712",
+            borderRadius: "8px",
+            width: "10px",
+            right: "2px",
+          },
+        }}
+        thumbYProps={{
+          style: {
+            background: "#3A6D8C",
+            borderRadius: "8px",
+          },
+        }}
+      >
+
+        <App />
+      </Scrollbar>
     </HelmetProvider>
   </StrictMode>,
 )
